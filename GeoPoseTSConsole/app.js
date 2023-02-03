d9d52d84dfde67e3bbb3bb8ab1b233d864a9bc5a
3DXScape <steve@opensiteplan.org>
2023-02-03 10:12:31 -0800

    Progressing
    Separated FrameTransform module

--- GeoPoseTSConsole/app.js
+++ GeoPoseTSConsole/app.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Translation = exports.GeodeticToEnu = exports.WGS84ToLTPENU = exports.Extrinsic = exports.PoseID = exports.Quaternion = exports.YPRAngles = exports.Advanced = exports.Local = exports.BasicQuaternion = exports.BasicYPR = void 0;
+exports.PoseID = exports.Quaternion = exports.YPRAngles = exports.Advanced = exports.Local = exports.BasicQuaternion = exports.BasicYPR = void 0;
 const node_process_1 = require("node:process");
 const proj4 = require("proj4");
 const Position = require("./Position");
+const FrameTransform = require("./FrameTransform");
 const LTPENU = require("./WGS84ToLTPENU");
 var source = proj4.Proj('EPSG:4326'); //source coordinates will be in Longitude/Latitude, WGS84
 var dest = proj4.Proj('EPSG:3785'); //destination coordinates in meters, global spherical mercators projection, see http://spatialreference.org/ref/epsg/3785/
@@ -52,7 +53,7 @@ class BasicYPR extends Basic {
     constructor(id, tangentPoint, yprAngles) {
         super();
         this.poseID = new PoseID(id);
-        this.FrameTransform = new WGS84ToLTPENU(tangentPoint);
+        this.FrameTransform = new FrameTransform.WGS84ToLTPENU(tangentPoint);
         this.Orientation = yprAngles;
     }
 }
@@ -67,7 +68,7 @@ class BasicQuaternion extends Basic {
     constructor(id, tangentPoint, quaternion) {
         super();
         this.poseID = new PoseID(id);
-        this.FrameTransform = new WGS84ToLTPENU(tangentPoint);
+        this.FrameTransform = new FrameTransform.WGS84ToLTPENU(tangentPoint);
         this.Orientation = quaternion;
     }
 }
@@ -226,124 +227,6 @@ class PoseID {
     }
 }
 exports.PoseID = PoseID;
-/// <summary>
-/// A FrameTransform is a generic container for information that defines mapping between reference frames.
-/// Most transformation have a context with necessary ancillary information
-/// that parameterizes the transformation of a Position in one frame to a corresponding Position is another.
-/// Such context may include, for example, some or all of the information that may be conveyed in an ISO 19111 CRS specification
-/// or a proprietary naming, numbering, or modelling scheme as used by EPSG, NASA Spice, or SEDRIS SRM.
-/// Subclasses of FrameTransform exist precisely to hold this context in conjunction with code
-/// implementing a Transform function.
-/// <remark>
-/// </remark>
-/// </summary>
-class FrameTransform {
-}
-/// <summary>
-/// A FrameSpecification is a generic container for information that defines a reference frame.
-/// <remark>
-/// A FrameSpecification can be abstracted as a Position:
-/// The origin of the coordinate system associated with the frame is a Position and serves in that role
-/// in the Advanced GeoPose.
-/// The origin, is in fact the *only* distinguished Position associated with the coodinate system.
-/// </remark>
-/// </summary>
-class Extrinsic extends FrameTransform {
-    constructor(authority, id, parameters) {
-        super();
-        this.authority = authority;
-        this.id = id;
-        this.parameters = parameters;
-    }
-    /// <summary>
-    /// The core function of a transformation is to implement a specific frame transformation
-    /// i.e. the transformation of a triple of point coordinates in the outer frame to a triple of point coordinates in the inner frame.
-    /// When this is not possible due to lack of an appropriate tranformation procedure,
-    /// the triple (NaN, NaN, NaN) [three IEEE 574 not-a-number vales] is returned.
-    /// Note that an "authority" is not necessarily a standards organization but rather an entity that provides
-    /// a register of some kind for a category of frame- and/or frame transform specifications that is useful and stable enough
-    /// for someone to implement transformation functions.
-    /// An implementation need not implement all possbile transforms.
-    /// </summary>
-    /// <note>
-    /// This would be a good element to implement as a set of plugin.
-    /// </note>
-    /// <param name="point"></param>
-    /// <returns></returns>
-    Transform(point) {
-        let uri = this.authority.toLowerCase().replace("//www.", "");
-        if (uri == "https://proj.org" || uri == "https://osgeo.org") {
-            var outer = proj4.Proj('EPSG:4326'); //source coordinates will be in Longitude/Latitude, WGS84
-            var inner = proj4.Proj('EPSG:3785'); //destination coordinates in meters, global spherical mercato
-            var cp = point;
-            let p = proj4.Point(cp.x, cp.y, cp.z);
-            proj4.transform(outer, inner, p);
-            // convert points from one coordinate system to another
-            let outP = new Position.CartesianPosition(p.x, p.y, p.z);
-            return outP;
-        }
-        else if (uri == "https://epsg.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://iers.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://naif.jpl.nasa.gov") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://sedris.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://iau.org") {
-            return Position.NoPosition;
-        }
-        return Position.NoPosition;
-    }
-}
-exports.Extrinsic = Extrinsic;
-Extrinsic.noTransform = new Position.NoPosition();
-/// <summary>
-/// A specialized specification of the WGS84 (EPSG 4326) geodetic frame to a local tangent plane East, North, Up frame.
-/// <remark>
-/// The origin of the coordinate system associated with the frame is a Position - the origin -
-/// which is the *only* distinguished Position associated with the coodinate system associated with the inner frame (range).
-/// </remark>
-/// </summary>
-class WGS84ToLTPENU extends FrameTransform {
-    constructor(origin) {
-        super();
-        this.Origin = origin;
-    }
-    Transform(point) {
-        let geoPoint = point;
-        let outPoint;
-        GeodeticToEnu(this.Origin, geoPoint, outPoint);
-        return outPoint;
-    }
-}
-exports.WGS84ToLTPENU = WGS84ToLTPENU;
-function GeodeticToEnu(origin, geoPoint, enuPoint) {
-    let out = new Position.CartesianPosition(0, 0, 0);
-    return out;
-}
-exports.GeodeticToEnu = GeodeticToEnu;
-// A simple translation frame transform.
-// The FrameTransform is created with an offset.
-// The Transform adds the offset ot an input Cartesian Position and reurns a Cartesian Position
-class Translation extends FrameTransform {
-    constructor(xOffset, yOffset, zOffset) {
-        super();
-        this.xOffset = xOffset;
-        this.yOffset = yOffset;
-        this.zOffset = zOffset;
-    }
-    Transform(point) {
-        let cp = point;
-        let p = new Position.CartesianPosition(cp.x + this.xOffset, cp.y + this.yOffset, cp.z + this.zOffset);
-        return p;
-    }
-}
-exports.Translation = Translation;
 let myLocal = new BasicYPR("OS_GB", new Position.GeodeticPosition(51.5, -1.5, 0.0), new YPRAngles(0, 0, 0));
 node_process_1.stdin.read();
 //# sourceMappingURL=app.js.map
--- GeoPoseTSConsole/app.ts
+++ GeoPoseTSConsole/app.ts
@@ -1,6 +1,7 @@
 import { stdin as input } from 'node:process';
 import * as proj4 from 'proj4';
 import * as Position from './Position';
+import * as FrameTransform from './FrameTransform';
 import * as LTPENU from './WGS84ToLTPENU';
 
 
@@ -55,7 +56,7 @@ abstract class GeoPose {
     // Optional and non-standard (except in Advanced) but conforming added property:
     // a validTime with milliseconds of Unix time.
     public validTime: number;
-    abstract FrameTransform: FrameTransform;
+    abstract FrameTransform: FrameTransform.FrameTransform;
     abstract Orientation: Orientation;
 }
 /// <summary>
@@ -68,7 +69,7 @@ abstract class Basic extends GeoPose {
     /// usually an ellipsoid of revolution or a gravitational equipotential surface is
     /// transformed to a local Cartesian frame, suitable for use over an extent of a few km.
     /// </summary>
-    public override FrameTransform: WGS84ToLTPENU;
+    public override FrameTransform: FrameTransform.WGS84ToLTPENU;
 }
 
 /// <summary>
@@ -78,7 +79,7 @@ export class BasicYPR extends Basic {
     public constructor(id: string, tangentPoint: Position.GeodeticPosition, yprAngles: YPRAngles) {
         super();
         this.poseID = new PoseID(id);
-        this.FrameTransform = new WGS84ToLTPENU(tangentPoint);
+        this.FrameTransform = new FrameTransform.WGS84ToLTPENU(tangentPoint);
         this.Orientation = yprAngles;
     }
     /// <summary>
@@ -97,7 +98,7 @@ export class BasicQuaternion extends Basic {
     public constructor(id: string, tangentPoint: Position.GeodeticPosition, quaternion: Quaternion) {
         super();
         this.poseID = new PoseID(id);
-        this.FrameTransform = new WGS84ToLTPENU(tangentPoint);
+        this.FrameTransform = new FrameTransform.WGS84ToLTPENU(tangentPoint);
         this.Orientation = quaternion;
     }
 
@@ -118,7 +119,7 @@ export class BasicQuaternion extends Basic {
 /// </remark>
 /// </summary>
 export class Local extends GeoPose {
-    public constructor(id: string, frameTransform: Translation, orientation: Quaternion) {
+    public constructor(id: string, frameTransform: FrameTransform.Translation, orientation: Quaternion) {
         super();
         this.poseID = new PoseID(id);
         this.FrameTransform = frameTransform;
@@ -127,7 +128,7 @@ export class Local extends GeoPose {
     /// <summary>
     /// The xOffset, yOffset, zOffset from the origin of the rotated inner frame of a "parent" GeoPose.
     /// </summary>
-    public override FrameTransform: Translation;
+    public override FrameTransform: FrameTransform.Translation;
 
     /// <summary>
     /// An Orientation specified as three rotations.
@@ -139,7 +140,7 @@ export class Local extends GeoPose {
 /// Advanced GeoPose.
 /// </summary>
 export class Advanced extends GeoPose {
-    public constructor(poseID: PoseID, frameTransform: Extrinsic, orientation: Quaternion) {
+    public constructor(poseID: PoseID, frameTransform: FrameTransform.Extrinsic, orientation: Quaternion) {
         super();
         this.poseID = poseID;
         this.FrameTransform = frameTransform;
@@ -149,7 +150,7 @@ export class Advanced extends GeoPose {
     /// <summary>
     /// A Frame Specification defining a frame with associated coordinate system whose Position is the origin.
     /// </summary>
-    public override FrameTransform: Extrinsic;
+    public override FrameTransform: FrameTransform.Extrinsic;
 
     /// <summary>
     /// An Orientation specified as a unit quaternion.
@@ -321,148 +322,6 @@ export class PoseID {
         this.id = id;
     }
     public id: string;
-}
-
-/// <summary>
-/// A FrameTransform is a generic container for information that defines mapping between reference frames.
-/// Most transformation have a context with necessary ancillary information
-/// that parameterizes the transformation of a Position in one frame to a corresponding Position is another.
-/// Such context may include, for example, some or all of the information that may be conveyed in an ISO 19111 CRS specification
-/// or a proprietary naming, numbering, or modelling scheme as used by EPSG, NASA Spice, or SEDRIS SRM.
-/// Subclasses of FrameTransform exist precisely to hold this context in conjunction with code
-/// implementing a Transform function.
-/// <remark>
-/// </remark>
-/// </summary>
-abstract class FrameTransform {
-    public abstract Transform(point: Position.Position): Position.Position;
-}
-
-/// <summary>
-/// A FrameSpecification is a generic container for information that defines a reference frame.
-/// <remark>
-/// A FrameSpecification can be abstracted as a Position:
-/// The origin of the coordinate system associated with the frame is a Position and serves in that role
-/// in the Advanced GeoPose.
-/// The origin, is in fact the *only* distinguished Position associated with the coodinate system.
-/// </remark>
-/// </summary>
-export class Extrinsic extends FrameTransform {
-    public constructor(authority: string, id: string, parameters: string) {
-        super();
-        this.authority = authority;
-        this.id = id;
-        this.parameters = parameters;
-    }
-    /// <summary>
-    /// The core function of a transformation is to implement a specific frame transformation
-    /// i.e. the transformation of a triple of point coordinates in the outer frame to a triple of point coordinates in the inner frame.
-    /// When this is not possible due to lack of an appropriate tranformation procedure,
-    /// the triple (NaN, NaN, NaN) [three IEEE 574 not-a-number vales] is returned.
-    /// Note that an "authority" is not necessarily a standards organization but rather an entity that provides
-    /// a register of some kind for a category of frame- and/or frame transform specifications that is useful and stable enough
-    /// for someone to implement transformation functions.
-    /// An implementation need not implement all possbile transforms.
-    /// </summary>
-    /// <note>
-    /// This would be a good element to implement as a set of plugin.
-    /// </note>
-    /// <param name="point"></param>
-    /// <returns></returns>
-    public override Transform(point: Position.Position): Position.Position {
-        let uri = this.authority.toLowerCase().replace("//www.", "");
-        if (uri == "https://proj.org" || uri == "https://osgeo.org") {
-            var outer = proj4.Proj('EPSG:4326');    //source coordinates will be in Longitude/Latitude, WGS84
-            var inner = proj4.Proj('EPSG:3785');     //destination coordinates in meters, global spherical mercato
-            var cp = point as Position.CartesianPosition;
-            let p = proj4.Point(cp.x, cp.y, cp.z);
-            proj4.transform(outer, inner, p);
-            // convert points from one coordinate system to another
-            let outP = new Position.CartesianPosition(p.x, p.y, p.z);
-            return outP;
-        }
-        else if (uri == "https://epsg.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://iers.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://naif.jpl.nasa.gov") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://sedris.org") {
-            return Position.NoPosition;
-        }
-        else if (uri == "https://iau.org") {
-            return Position.NoPosition;
-        }
-        return Position.NoPosition;
-    }
-    /// <summary>
-    /// The name or identification of the definer of the category of frame specification.
-    /// A Uri that usually but not always points to a valid web address.
-    /// </summary>
-    public authority: string;
-    /// <summary>
-    /// A string that uniquely identifies a frame type.
-    /// The interpretation of the string is determined by the authority.
-    /// </summary>
-    public id: string;
-    /// <summary>
-    /// A string that holds any parameters required by the authority to define a frame of the given type as specified by the id.
-    /// The interpretation of the string is determined by the authority.
-    /// </summary>
-    public parameters: string;
-    public static noTransform: Position.Position = new Position.NoPosition();
-}
-/// <summary>
-/// A specialized specification of the WGS84 (EPSG 4326) geodetic frame to a local tangent plane East, North, Up frame.
-/// <remark>
-/// The origin of the coordinate system associated with the frame is a Position - the origin -
-/// which is the *only* distinguished Position associated with the coodinate system associated with the inner frame (range).
-/// </remark>
-/// </summary>
-export class WGS84ToLTPENU extends FrameTransform {
-    public constructor(origin: Position.GeodeticPosition) {
-        super();
-        this.Origin = origin;
-    }
-    public override Transform(point: Position.Position): Position.Position {
-        let geoPoint = point as Position.GeodeticPosition;
-        let outPoint: Position.CartesianPosition;
-        GeodeticToEnu(this.Origin, geoPoint, outPoint);
-        return outPoint;
-    }
-
-    /// <summary>
-    /// A single geodetic position defines the tangent point for a transform to LTP-ENU.
-    /// </summary>
-    public Origin: Position.GeodeticPosition;
-}
-
-export function GeodeticToEnu(origin: Position.GeodeticPosition, geoPoint: Position.GeodeticPosition, enuPoint: Position.CartesianPosition) {
-    let out = new Position.CartesianPosition(0, 0, 0);
-    return out;
-}
-
-// A simple translation frame transform.
-// The FrameTransform is created with an offset.
-// The Transform adds the offset ot an input Cartesian Position and reurns a Cartesian Position
-export class Translation extends FrameTransform {
-    public constructor(xOffset: number, yOffset: number, zOffset: number) {
-        super();
-        this.xOffset = xOffset;
-        this.yOffset = yOffset;
-        this.zOffset = zOffset;
-    }
-    public override Transform(point: Position.Position): Position.Position {
-        let cp = point as Position.CartesianPosition;
-        let p = new Position.CartesianPosition(cp.x + this.xOffset, cp.y + this.yOffset, cp.z + this.zOffset);
-        return p;
-    }
-    public xOffset: number;
-    public yOffset: number;
-    public zOffset: number;
 }
 
 
